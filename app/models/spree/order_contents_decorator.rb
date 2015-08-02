39401f449a1c24c11e8b0fdd93ec7c0ae225bfba
Wooobee <m.laux@wooobee.com>
2015-08-02 18:23:58 +0200

    update

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -2,18 +2,7 @@ Spree::OrderContents.class_eval do
 
   def add(variant, quantity = 1, options = {})
     line_item = add_to_line_item(variant, quantity, options)
-    if options[:quick_add]
-      simple_after_add_or_remove(line_item, options)
-    else
-      after_add_or_remove(line_item, options)
-    end
-  end
-
-  private
-
-  def simple_after_add_or_remove(line_item, options = {})
-    reload_totals
-    line_item
+    after_add_or_remove(line_item, options)
   end
 
 end
